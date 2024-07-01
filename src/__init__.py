e372a01290023d72f011fb6d6263430263392191
Kelly Ton <kthton@gmail.com>
2024-07-01 12:13:10 -0400

    Updated __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,11 @@
 # Import utils.py functions
-from src.utils import load_json
+from src.utils import load_json, save_to_json
 
 # Import normalization.py functions
 from src.normalization import load_spacy_model, translate_text, preprocess_text, preprocess_data
 
 # Import sentiment_analysis.py functions
 from src.sentiment_analysis import sentiment_analyzer
+
+# Import data_visualizations.py functions
+from src.data_visualization import print_sentiment_analysis, visualize_sentiment
